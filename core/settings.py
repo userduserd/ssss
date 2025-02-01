2c5c1615f65ef80fb8c973ab8468f74200e7a08f
userduserd <dino_server@proton.me>
2025-02-01 20:59:52 +0600

    fdsg

--- core/settings.py
+++ core/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-ynzk8+(sgf8fk#*g307wlrkgiyd5puh505@kmk)jnd3nh@=cif
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+load_dotenv()
+ALLOWED_HOSTS = [os.getenv("ALLOW_HOST")]
 
 
 # Application definition
